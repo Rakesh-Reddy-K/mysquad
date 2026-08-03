f320c23a7f193c5a2c765984c5b30d8a514d7a1f
Rakesh Reddy <rakeshreddyk6312@outlook.com>
2026-08-03 12:51:49 +0530

    Fix frontend proxy: custom entrypoint with targeted envsubst on \ only

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Substitute only BACKEND_URL in the template; leave nginx runtime
-# variables ($host, $uri, $remote_addr, etc.) untouched.
 set -e
 
+# Only substitute $BACKEND_URL in the template.
+# This preserves all nginx runtime variables ($host, $remote_addr, etc.)
+# which would otherwise be wiped to empty strings by a blanket envsubst.
 envsubst '$BACKEND_URL' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
-# Hand off to the default nginx entrypoint (exec nginx -g "daemon off;")
-exec /docker-entrypoint.sh nginx -g "daemon off;"
+exec nginx -g "daemon off;"
